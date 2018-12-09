85de666c0231b3d0c8e1317ef9ee1fe06d4d844e
Sunghyun <anaclumos@gmail.com>
2018-12-09 22:16:30 +0900

    Initial Files

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Automated-Roadsign-System
-Automated Roadsign System with Efficient Traffic Management and Audial Assistance, simulated with Arduion Uno and Custom Designed Circuit Board.
+# Automated Roadsign System
+Automated Roadsign System with
+
+* Efficient Traffic Management
+* Audial Assistance
+
+simulated with
+
+* Arduino Uno
+* Custom Designed Circuit Board (Designed by me)
+
+### Note: This project is from June ~ July 2017.
+
+If you want to check the **codes**, 
+### [`View Codes`](RoadSign.ino)
+
+If you want to get the **idea** of the project, see
+### [`Mid Presentation PDF`](Mid-Presentation.pdf)
+
+If you want to see the **results**, see
+### [`Fin Presentation PDF`](Fin-Presentation.pdf)
+
+or you can also see...
+### [`Demo Video`](IMG_0139.mp4)
+
+# If you are a Mac user...
+### [`Mid Presentation Keynote`](Mid-Presentation.key)
+### [`Fin Presentation Keynote`](Fin-Presentation.key)
---
+++ RoadSign.ino
@@ -0,0 +1,84 @@
+boolean val1, val2;
+const int laser1 = 2;
+const int led1gre = 3;
+const int led1leg = 4;
+const int led1red = 5;
+const int buz1gro = 6;
+const int buz1sky = 7;
+const int buz2gro = 8;
+const int buz2sky = 9;
+const int led2gre = 10;
+const int led2leg = 11;
+const int led2red = 12;
+const int laser2 = 13;
+
+void setup ()
+{
+   Serial.begin(9600);
+   pinMode(laser1, INPUT);
+   pinMode(laser2, INPUT);
+   pinMode(led1gre, OUTPUT);
+   pinMode(led1leg, OUTPUT);
+   pinMode(led1red, OUTPUT);
+   pinMode(buz1gro, OUTPUT);
+   pinMode(buz1sky, OUTPUT);
+   pinMode(buz2gro, OUTPUT);
+   pinMode(buz2sky, OUTPUT);
+   pinMode(led2gre, OUTPUT);
+   pinMode(led2leg, OUTPUT);
+   pinMode(led2red, OUTPUT);
+   digitalWrite(led1gre, LOW);
+   digitalWrite(led1leg, LOW);
+   digitalWrite(led1red, HIGH);
+   digitalWrite(buz1gro, LOW);
+   digitalWrite(buz1sky, LOW);
+   digitalWrite(buz2gro, LOW);
+   digitalWrite(buz2sky, LOW);
+   digitalWrite(led2gre, LOW);
+   digitalWrite(led2leg, LOW);
+   digitalWrite(led2red, HIGH);
+}
+
+void loop () {
+  
+   val1 = digitalRead(laser1);
+   val2 = digitalRead(laser2);
+   Serial.println("val1 : "+(String)val1 + " val2 : " +(String)val2);
+  
+   if(!val1) {
+      Serial.println("RoadSign 1 detected an object");
+      digitalWrite(buz1sky, HIGH); delay(500);
+      digitalWrite(buz1sky, LOW); delay(1000);
+      for(int i = 0; i < 5; i++){
+        digitalWrite(led1red, LOW); digitalWrite(led1gre, HIGH);
+        digitalWrite(led2red, LOW); digitalWrite(led2gre, HIGH);
+        digitalWrite(buz1sky, HIGH); delay(300); digitalWrite(buz1sky, LOW); delay(300);
+        digitalWrite(buz2sky, HIGH); delay(140); digitalWrite(buz2sky, LOW); delay(20);
+        digitalWrite(buz2sky, HIGH); delay(140); digitalWrite(buz2sky, LOW); delay(20);
+        delay(300);
+      }
+   }
+   
+   else
+   {
+     digitalWrite(led1gre, LOW); digitalWrite(led1red, HIGH);
+   }
+
+      if(!val2) {
+      Serial.println("RoadSign 2 detected an object");
+      digitalWrite(buz2sky, HIGH); delay(140); digitalWrite(buz2sky, LOW); delay(20);
+      digitalWrite(buz2sky, HIGH); delay(140); digitalWrite(buz2sky, LOW); delay(3000);
+      for(int i = 0; i < 5; i++){
+        digitalWrite(led2red, LOW); digitalWrite(led2gre, HIGH);
+        digitalWrite(led1red, LOW); digitalWrite(led1gre, HIGH); 
+        digitalWrite(buz2sky, HIGH); delay(140); digitalWrite(buz2sky, LOW); delay(20);
+        digitalWrite(buz2sky, HIGH); delay(140); digitalWrite(buz2sky, LOW); delay(300);
+        digitalWrite(buz1sky, HIGH); delay(300); digitalWrite(buz1sky, LOW); delay(300);
+      }
+   }
+   
+   else
+   {
+     digitalWrite(led2gre, LOW); digitalWrite(led2red, HIGH);
+   }
+}
